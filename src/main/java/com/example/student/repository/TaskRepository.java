b24b3877daf46001174149b349ca3ba1ae999df5
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-02-08 22:04:54 +0100

    TaskRepository - rozszerzenie Jpa

---
+++ src/main/java/com/example/student/repository/TaskRepository.java
@@ -0,0 +1,25 @@
+package com.example.student.repository;
+
+import com.example.student.model.TaskModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<TaskModel, Long> {
+
+    List<TaskModel> findByColor(String name);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE TaskModel t SET t.description = :description WHERE t.id = :id")
+    int updateDescriptionById(@Param("id") Long id, @Param("description") String description);
+
+
+
+}
