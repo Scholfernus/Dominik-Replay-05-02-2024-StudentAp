ee6b7ec68646e3f7c6006c9605b59235406f89e2
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-03-01 21:25:20 +0100

    AuthConfig - klasa konfiguracyjna z dosepami do admina etp.

---
+++ src/main/java/com/example/student/config/AuthConfig.java
@@ -0,0 +1,59 @@
+package com.example.student.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+public class AuthConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public InMemoryUserDetailsManager get() {
+        UserDetails user = User.withUsername("test")
+                .password(passwordEncoder().encode("test"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(Arrays.asList(user, admin));
+    }
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .antMatchers("/students").hasAnyRole("USER")
+                .antMatchers("/tasks").hasAnyRole("ADMIN")
+                .antMatchers("/").permitAll()
+                .and()
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .formLogin() // teraz będę konfigurował formularz autoryzacji
+                .loginPage("/login")
+                .usernameParameter("username") // nadajemy nazwę jaka będzie jako name w inpucie loginu formularza
+                .passwordParameter("password")// nadajemy nazwę jaka będzie jako name w inpucie hasła formularza
+                .loginProcessingUrl("/login")
+                .failureForwardUrl("/login?error") // co sięstanie w momencie wpisania błędnych danych
+                .defaultSuccessUrl("/")// co sięstanie w momencie prawidłowego uwierzytelnienia
+                .and()
+                .logout() // mówimy springowi, że przechodzimy do obsłużenia logout
+                .logoutSuccessUrl("/login")
+                .logoutUrl("/logout");
+        return http.build();
+    }
+}
